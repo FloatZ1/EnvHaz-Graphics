2852cad256e48d30cb85fddb717235297fe3bce8
FloatZ1 <80592580+FloatZ1@users.noreply.github.com>
2025-11-18 14:35:42 +0200

    Update Window.cpp

--- lib/EnvHazGraphics/src/Window.cpp
+++ lib/EnvHazGraphics/src/Window.cpp
@@ -5,120 +5,58 @@
 #include <cstdlib>
 #include <glad/glad.h>
 
+namespace eHazGraphics {
 
+bool Window::Create(int width, int height, bool fullscreen, std::string title) {
+  mHeight = height;
+  mWidth = width;
 
+  // ---- NEW: set GL attributes BEFORE creating window/context ----
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+  // ----------------------------------------------------------------
 
+  bool success = true;
+  mWindow = SDL_CreateWindow(title.c_str(), width, height,
+                             SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE |
+                                 SDL_WINDOW_HIGH_PIXEL_DENSITY);
+  if (!mWindow) {
+    SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
+    success = false;
+  } else {
+    glContext = SDL_GL_CreateContext(mWindow);
 
-
-
-
-
-
-
-
-
-namespace eHazGraphics
-{
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-bool Window::Create(int width, int height, bool fullscreen, std::string title)
-{
-    mHeight = height;
-    mWidth = width;
-
-    // ---- NEW: set GL attributes BEFORE creating window/context ----
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
-    // ----------------------------------------------------------------
-
-    bool success = true;
-    mWindow = SDL_CreateWindow(title.c_str(), width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-    if (!mWindow)
-    {
-        SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
-        success = false;
+    if (!glContext) {
+      SDL_Log("ERROR: COULD NOT CREATE OpenGL CONTEXT.");
+      success = false;
+    } else {
+      SDL_GL_MakeCurrent(mWindow, glContext);
+      SDL_Log("OpenGL context created successfully");
     }
-    else
-    {
-        glContext = SDL_GL_CreateContext(mWindow);
+  }
 
-        if (!glContext)
-        {
-            SDL_Log("ERROR: COULD NOT CREATE OpenGL CONTEXT.");
-            success = false;
-        }
-        else
-        {
-            SDL_GL_MakeCurrent(mWindow, glContext);
-            SDL_Log("OpenGL context created successfully");
-        }
-    }
-
-
-
-
-
-    return success;
+  return success;
 }
-
 
 bool previousCursorStat = false;
 
-void Window::Update()
-{
-    // if (lockCursor != previousCursorStat)
-    {
+void Window::Update() {
+  // if (lockCursor != previousCursorStat)
+  {
 
+    SDL_SetWindowRelativeMouseMode(mWindow, lockCursor);
+    // SDL_WarpMouseInWindow(mWindow, std::abs(mWidth / 2), std::abs(mHeight /
+    // 2));
 
-        SDL_SetWindowRelativeMouseMode(mWindow, lockCursor);
-        // SDL_WarpMouseInWindow(mWindow, std::abs(mWidth / 2), std::abs(mHeight / 2));
-
-
-
-
-        previousCursorStat = lockCursor;
-    }
+    previousCursorStat = lockCursor;
+  }
 }
 
-
-
-
-
-
-
-
-SDL_Window *Window::GetWindowPtr() const
-{
-    return mWindow;
-}
-SDL_GLContext Window::GetOpenGLContext()
-{
-    return glContext;
-}
-int Window::GetWidth() const
-{
-    return mWidth;
-}
-int Window::GetHeight() const
-{
-    return mHeight;
-}
+SDL_Window *Window::GetWindowPtr() const { return mWindow; }
+SDL_GLContext Window::GetOpenGLContext() { return glContext; }
+int Window::GetWidth() const { return mWidth; }
+int Window::GetHeight() const { return mHeight; }
 
 } // namespace eHazGraphics
